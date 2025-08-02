3427ab5c8ded4c87c03060e71d9a2b71dba6e842
angel <angelicaizabella90@gmail.com>
2025-08-02 22:57:04 +0700

    update background

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { AuthContext } from "./context/AuthContext";
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('/src/assets/bgImage.svg')] bg-contain bg-center bg-no-repeat min-h-screen bg-black">
+    <div className="bg-[url('/bgImage.svg')] bg-contain bg-center bg-no-repeat min-h-screen bg-black">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> :<Navigate to="/login"/>}/>
